e587f3762bff13192be062108a62acfda550fd02
troj0 <mu8@tuta.io>
2025-04-04 22:22:33 +0300

    fix: tailwind load fail

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-    "./lib/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./lib/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {},
   },
+  safelist: [
+    'bg-gray-900',
+    'text-gray-100',
+    'bg-gray-800',
+    'bg-gray-950',
+    'text-purple-400',
+    'text-blue-400',
+    'text-yellow-400',
+    'text-green-400',
+    'text-red-400',
+  ],
   plugins: [],
 } 
